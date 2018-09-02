d222f04913843baaa1781c359ea0b9df3f9751cf
Frank <pham.francesco@gmail.com>
2018-09-02 23:00:19 +0200

    new script that print bandwidth usage

---
+++ solusvm-bwused.py
@@ -0,0 +1,34 @@
+import requests
+import re
+
+def humanFileSize(size):
+    if(size >= 1<<40):
+        size = size/(1<<40)
+        return '%.2f' % size + ' TiB'
+    if(size >= 1<<30):
+        size = size/(1<<30)
+        return '%.2f' % size + ' GiB'
+    if(size >= 1<<20):
+        size = size/(1<<20)
+        return '%.2f' % size + ' MiB'
+    if(size >= 1<<10):
+        size = size/(1<<10)
+        return '%.2f' % size + ' KiB'
+    return '%.2f' % size + ' Bytes'
+
+key = ''
+hash = ''
+url = ''
+
+payload = {'key':key,
+        'hash':hash,
+        'action':'info',
+        'bw':'true'}
+r = requests.post(url+'/api/client/command.php', data=payload, verify=False)
+match = re.match(r'<bw>(\d*),(\d*),(\d*),(\d*)<\/bw>', r.text)
+
+total = int(match.group(1))
+used = int(match.group(2))
+percent = used/total*100
+
+print('  Data usage for this month: '+humanFileSize(used)+' of '+humanFileSize(total)+' used. ('+'%.3f' %percent+' %)')
